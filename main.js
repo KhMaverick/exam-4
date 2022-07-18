a9b88899b4dc855193f413dcdbf4c8572898dd46
KhMaverick <xusanqodir@gmail.com>
2022-07-18 14:48:49 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// $("#Send").on("click", ()=>{
+//     $.ajax({
+//         url: "https://reqres.in/api/users-2",
+//         type: "POST",
+//         data: {
+//             page: $("#page").val(),
+//             year: $("#year").val()
+//         },
+//         // data: $("form").serialize(),
+//         success: (value)=>{
+//             console.log(value);
+//         }
+//     })
+// })
